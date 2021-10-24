3fadacad9bd46c1cc5128f29ed043aa70ec5f115
c-yator <christian.yator@students.jkuat.ac.ke>
2021-10-24 11:18:14 +0300

    added canvas logic

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ import 'react-toastify/dist/ReactToastify.css';
 import NavBar from './components/NavBar';
 import FooterPortal from './components/FooterPortal';
 import Home from './components/Home';
+import Canvas from './components/Canvas';
 
 function App() {
 	return (
 		<div className="App">
+			{/* <Canvas /> */}
 			<NavBar />
 			<Home />
 			<FooterPortal />
